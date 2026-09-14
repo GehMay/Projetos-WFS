d3fab2d93d52235744be4018b16377feb9c07d4e
Geovanna Tamagusko <geovannavmayumi@gmail.com>
2026-09-14 10:54:56 -0300

    Aula 6 e inicialização da aula 7

---
+++ Aula-06/starter/client/src/components/Teste.jsx
@@ -0,0 +1,9 @@
+function Teste(){
+    return(
+        <>
+        <h1>Meu Título</h1>
+        <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur quasi labore asperiores adipisci cum voluptates animi, blanditiis ratione quis recusandae nobis vero vitae nulla! Ullam atque dolores consequuntur nisi natus?</p>
+        </>
+    )
+}
+export default Teste
